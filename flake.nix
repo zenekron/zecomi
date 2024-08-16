a241b605c9023ea6a446b08ca021072eb2ab6f91
zenekron <zenekron@gmail.com>
2024-08-16 18:04:52 +0200

    chore: create nix flake and load dev environment using direnv

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs_18
+              corepack_18
+
+              nixd
+              nixpkgs-fmt
+            ];
+          };
+        }
+      )
+  ;
+}
