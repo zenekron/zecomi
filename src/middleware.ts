93fda42e0777b41aef64e2514b43df44474b0aa8
zenekron <zenekron@gmail.com>
2024-08-31 17:57:47 +0200

    docs: provide some barebones documentation for the exported types and their methods

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,16 @@ type _Middleware<T extends (...args: never[]) => unknown> = {
   invoke: T;
 };
 
+/**
+ * A `Middleware` is a component that sits between a `Service` and the outside,
+ * it can observe, manipulate or intercept requests coming from the outside and
+ * usually has to rely on the underlying `Service` to process them.
+ */
 export interface Middleware<EIn, EOut, IIn = EIn, IOut = EOut>
   extends _Middleware<(input: EIn, next: Service<IIn, IOut>) => EOut> {}
 
-export interface AsyncMiddleware<EIn, EOut, IIn = EIn, IOut=EOut>
+/**
+ * A {@see Middleware} that returns `Promise`s.
+ */
+export interface AsyncMiddleware<EIn, EOut, IIn = EIn, IOut = EOut>
   extends Middleware<EIn, Promise<EOut>, IIn, Promise<IOut>> {}
--- src/service.ts
+++ src/service.ts
@@ -10,13 +10,27 @@ interface _Service<T extends (...args: never[]) => unknown> {
   invoke: T;
 }
 
+/**
+ * A `Service` is an object that is capable of consuming inputs of type `I` and
+ * producing outputs of type `O`.
+ */
 export interface Service<I, O> extends _Service<(input: I) => O> {}
 
+/**
+ * A builder for `Service`s, it lets you stack middlewares a fuse them together
+ * into new `Service`s.
+ */
 export class ServiceBuilder<EIn, EOut, IIn, IOut> {
+  /**
+   * Creates a new `ServiceBuilder` over the provided input and output types.
+   */
   public static create<I, O>(): ServiceBuilder<I, O, I, O> {
     return new ServiceBuilder<I, O, I, O>((service) => service);
   }
 
+  /**
+   * Same as {@link create} but for `Service`s that return `Promise`s.
+   */
   public static createAsync<I, O>(): ServiceBuilder<
     I,
     Promise<O>,
@@ -30,6 +44,10 @@ export class ServiceBuilder<EIn, EOut, IIn, IOut> {
     private readonly fn: (service: Service<IIn, IOut>) => Service<EIn, EOut>,
   ) {}
 
+  /**
+   * Add a middleware to the current stack, this middleware will run after all
+   * the previously added middlewares.
+   */
   public use<I, O>(
     middleware: Middleware<IIn, IOut, I, O>,
   ): ServiceBuilder<EIn, EOut, I, O> {
@@ -38,6 +56,10 @@ export class ServiceBuilder<EIn, EOut, IIn, IOut> {
     );
   }
 
+  /**
+   * Compiles the previously provided middlewares and the service `service` into
+   * a new `Service`.
+   */
   public build(service: Service<IIn, IOut>): Service<EIn, EOut> {
     return this.fn(service);
   }
