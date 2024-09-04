7559f37fc8c80da85dd8a5d12bee9a121087e15e
zenekron <zenekron@gmail.com>
2024-09-04 20:35:02 +0200

    test: verify that function middlewares and services behave okay

--- src/service-builder.test.ts
+++ src/service-builder.test.ts
@@ -4,6 +4,31 @@ import { ServiceBuilder } from "./service-builder.js";
 import type { Service } from "./service.js";
 
 describe("ServiceBuilder", () => {
+  it("allows providing functions instead of middlewares and services", async () => {
+    type WithFoo<T extends object> = T & { foo: string };
+    function withFoo<T extends object>(obj: T, foo: string): WithFoo<T> {
+      return Object.assign(obj, { foo });
+    }
+
+    const svc = ServiceBuilder.createAsync<Request, WithFoo<Response>>()
+      // no transforms
+      .use((input, next) => next.invoke(input))
+      // input transform
+      .use<WithFoo<Request>>((input, next) =>
+        next.invoke(withFoo(input, "bar")),
+      )
+      // output transform
+      .use<WithFoo<Request>, Promise<Response>>(async (input, next) =>
+        withFoo(await next.invoke(input), "baz"),
+      )
+      .build((input) => Promise.resolve(new Response(input.url)));
+
+    const input = new Request("https://example.local");
+    const output = await svc.invoke(input);
+    expect(await output.text()).toStrictEqual(input.url);
+    expect(output.foo).toBe("baz");
+  });
+
   it("chains middlewares in the correct order", () => {
     interface OrderInput {
       inputs: number[];
